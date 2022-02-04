fa64390bac22770130f144b10d8b4a8146810ce8
mmizumoto1213 <97698811+mmizumoto1213@users.noreply.github.com>
2022-02-04 13:40:52 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1, 2)); // Should Fail
+        assertEquals(3, SkillDemo.add(1, 2)); // Should Fail
     }
 }
